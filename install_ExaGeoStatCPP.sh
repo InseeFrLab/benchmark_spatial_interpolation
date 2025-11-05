6377c68f980506a4247098e4c36c7d4380af166e
oliviermeslin <olivier.meslin@insee.fr>
2025-11-05 13:47:10 +0000

    WIP

--- install_ExaGeoStatCPP.sh
+++ install_ExaGeoStatCPP.sh
@@ -1,22 +1,7 @@
 cd /home/onyxia/work/
 
 # Install libtool
-wget http://ftpmirror.gnu.org/libtool/libtool-2.4.7.tar.gz
-tar -xvzf libtool-2.4.7.tar.gz
-cd libtool-2.4.7
-./configure --prefix=$HOME/local
-make
-make install
-
-export PATH=$HOME/local/bin:$PATH
-export LD_LIBRARY_PATH=$HOME/local/lib:$LD_LIBRARY_PATH
-export PKG_CONFIG_PATH=$HOME/local/lib/pkgconfig:$PKG_CONFIG_PATH
-
-export LIBTOOL=$HOME/local/bin/libtool
-export LIBTOOLIZE=$HOME/local/bin/libtoolize
-
-export PKG_CONFIG_PATH=$HOME/local/lib/pkgconfig:$PKG_CONFIG_PATH
-export LD_LIBRARY_PATH=$HOME/local/lib:$LD_LIBRARY_PATH
+sudo apt install libtool libtool-bin
 
 # Clone the ExaGeoStatCPP repo
 cd /home/onyxia/work/
@@ -24,6 +9,10 @@ git clone https://github.com/ecrc/ExaGeoStatCPP.git
 
 # Run in R
 # install.packages(c("Rcpp", "assertthat"))
+
+# Install hwloc
+# sudo apt update
+# sudo apt install hwloc libhwloc-dev
 
 # Install ExaGeoStatCPP
 cd ExaGeoStatCPP
