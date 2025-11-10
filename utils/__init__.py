68ec181247c1f4da39bb4b7a952acccc82d14c0c
oliviermeslin <olivier.meslin@insee.fr>
2025-11-10 18:06:35 +0000

    Cleaning

--- utils/__init__.py
+++
@@ -1,4 +0,0 @@
-from .s3 import get_df_from_s3 as get_df_from_s3
-from .s3 import write_df_to_s3 as write_df_to_s3
-from .s3 import get_model_from_s3 as get_model_from_s3
-from .s3 import write_model_to_s3 as write_model_to_s3
